fe918522657549257e8d55f25802a5115d9c4c47
Lia001218 <liazerqueraf@gmail.com>
2022-03-25 14:44:02 -0700

    Update Structure.py

--- Structure.py
+++ Structure.py
@@ -1,30 +1,46 @@
 import NumPy
-class Node :
-    def __init__(self,name):
+
+
+class Node:
+    def __init__(self, name):
         self.name = name
         self.menssage = None
         self.history_operation = None
         self.instruction = None
+
+
 adj_list = {}
 mylist = []
-def Add_Node(node):
+
+
+def add_node(node):
     if(node not in mylist):
         mylist.append(node)
         adj_list.append(node)
     else:
-       print('esto tal vez lo quite, en caso que lo deje el mensage es : Node ','node,already exists')
-def Add_edge(node1,node2):
+        print('esto tal vez lo quite, en caso que lo deje el mensage es : Node ',
+              'node,already exists')
+
+
+def add_edge(node1, node2):
     if node1 in mylist and node2 in mylist:
         adj_list[node1].append(node2)
         adj_list[node2].append(node1)
     else:
         print('At least one of the nodes does not exist ')
-def Save_Message(node , message):
-    self.message = message
-def Add_History(node,action):
-    self.history.append(action)
-def Add_Instruction(node , instruction): # Implemetarlo como Heap que el valor con menor tiempo siempre este al principio
-    self.instruction.append(instruction)
-def Delete_Instruction(node):
-    instruction.remove()
 
+
+def save_message(node, message):
+    self.message = message
+
+
+def add_history(node, action):
+    self.history.append(action)
+
+# Implemetarlo como Heap que el valor con menor tiempo siempre este al principio
+def add_instruction(node, instruction):
+    self.instruction.append(instruction)
+
+
+def delete_instruction(node):
+    instruction.remove()
