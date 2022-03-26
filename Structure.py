32836c40bfa7de50acf3505165102be7d3d26f2f
Lia001218 <liazerqueraf@gmail.com>
2022-03-26 00:07:42 -0700

    Update Structure.py
    Funciona Structure bien

--- Structure.py
+++ Structure.py
@@ -1,31 +1,32 @@
-import NumPy
-
-
+# import NumPy
 class Node:
     def __init__(self, name):
         self.name = name
         self.menssage = None
         self.history_operation = None
         self.instruction = None
+        
+class Graph:
+    def __init__(self):
         self.adj_list = {}
         self.mylist = []
-
-    def add_Node(self, node):
-        if(node not in mylist):
+        
+    def add_node(self, node):
+        if(node not in self.mylist):
             self.mylist.append(node)
-            self.adj_list.append(node)
+            self.adj_list[node] = []
         else:
             print('esto tal vez lo quite, en caso que lo deje el mensage es : Node ',
                   'node,already exists')
 
     def add_edge(self, node1, node2):
-        if node1 in mylist and node2 in mylist:
+        if node1 in self.mylist and node2 in self.mylist:
             self.adj_list[node2].append(node1)
             self.adj_list[node1].append(node2)
         else:
             print('At least one of the nodes does not exist ')
 
-    def save_message(self.node, message):
+    def save_message(self ,node, message):
         self.message = message
 
     def add_history(self, node, action):
@@ -37,3 +38,20 @@ class Node:
 
     def delete_instruction(self, node):
         instruction.remove()
+    def graph(self):
+        for node in self.adj_list:
+            print(node, " --- ", [i for i in self.adj_list[node]])
+graph1  = Graph()
+
+graph1.add_node(0)
+graph1.add_node(1)
+graph1.add_node(2)
+graph1.add_node(3)
+graph1.add_node(4)
+graph1.add_edge(0,1)
+graph1.add_edge(1,2)
+graph1.add_edge(2,3)
+graph1.add_edge(3,0)
+graph1.add_edge(3,4)
+graph1.add_edge(4,0)
+graph1.graph()
